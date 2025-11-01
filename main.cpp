6a54e96d04584a7ce65c79c3ba80f68888b53f1d
elizazha <lizokbella@mail.ru>
2025-11-01 13:08:37 +0300

    first programm

--- main.cpp
+++ main.cpp
@@ -1,5 +1,66 @@
 #include <iostream>
+int ** create( size_t rows, size_cols)
+{
+  int ** mtx = new int * [rows];
+  try
+  {
+    for (sizw_t i = 0; i < rows; ++i)
+    {
+      mtx[i]=new int(cols);
+    }
+  }
+  catch(const std::bad_alloc&e}
+  {
+    #include <iostream>
+//хотим создать двумерый массив. в нашем слуаче 5х5
+void destroy(int** mtx, size_t created)
+{
+    for (size_t i = 0; i < created; i++)
+    {
+        delete[] mtx[i];
+    }
+    delete[] mtx;
+}
+
+int ** create(size_t rows, size_t cols)
+{
+    int** mtx = new int * [rows];
+    size_t created = 0; //счетчик чтобы посмотреть сколько массивров внутри массива успело выделиться
+    try
+    {
+        for (; created < rows;++created) //(size_t i = 0; i < rows; ++i)
+        {
+            mtx[created] = new int(cols);
+        }
+    }
+    catch (const std::bad_alloc& e)
+    {
+        destroy(mtx, created);
+        throw;//return nullptr;
+    }
+    return mtx;
+}
+
 int main()
 {
-    std::cout<<"Hello world!";
+    int** matrix = nullptr;
+    try {
+        matrix = create(5, 5);
+    }
+    catch (const std::bad_alloc& e)
+    {
+        std::cerr << e.what()<<"\n";
+    }
+    destroy(matrix, 5);
+}
+
+
+
+  }
+}
+
+int main()
+{
+  int ** matrix = nullptr;
+  matrix = create(5,5);
 }
