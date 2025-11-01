8164f3c31ee9b48a2ccc5d0829db7c3c13c7a9e6
elizazha <lizokbella@mail.ru>
2025-11-01 20:25:46 +0300

    fourth programm

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 //хотим создать двумерый массив. в нашем слуаче 5х5
 void destroy(int** mtx, size_t created)
 {
@@ -17,7 +18,7 @@ int** create(size_t rows, size_t cols)
     {
         for (; created < rows; ++created) //(size_t i = 0; i < rows; ++i)
         {
-            mtx[created] = new int(cols);
+            mtx[created] = new int[cols];
         }
     }
     catch (const std::bad_alloc& e)
@@ -40,7 +41,7 @@ void construct(int** mtx, int init, size_t rows, size_t cols)
 }
 
 void input(int ** mtx, size_t rows, size_t cols)
-{3
+{
     for (size_t i = 0; i < rows; i++)
     {
         for (size_t j = 0; j < cols; j++)
@@ -67,6 +68,54 @@ int ** create(...);
 void destroy();
 могли их объявить, а исп потом в main 
 */
+
+int** convert(const int* t, size_t n, const size_t* lns, size_t rows)
+{
+    // Проверяем, что общее количество элементов совпадает
+    size_t total_elements = 0;
+    for (size_t i = 0; i < rows; i++) {
+        total_elements += lns[i];
+    }
+
+    if (total_elements != n) {
+        throw std::invalid_argument("Total elements in lns doesn't match n");
+    }
+    // Создаем "рваный" массив
+    int** result = new int* [rows];
+    size_t line = 0;
+    size_t current_index_of_t = 0;
+    try {
+        for (; line < rows; ++line) {
+            // Создаем строку длиной lns[line]
+            result[line] = new int[lns[line]];
+
+            // Копируем данные из исходного массива
+            for (size_t j = 0; j < lns[line]; j++) {
+                result[line][j] = t[current_index_of_t];
+                current_index_of_t++;
+            }
+        }
+    }
+    catch (const std::bad_alloc& e) {
+        destroy(result, line);
+        throw;
+    }
+    return result;
+}
+
+void output_homework(int** mtx, const size_t* lns, size_t rows)
+{
+    for (size_t i = 0; i < rows; i++)
+    {
+        std::cout << i << lns[i]; //строка, длина
+        for (size_t j = 0; j < lns[i]; j++)
+        {
+            std::cout << mtx[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
 int main()
 {
     size_t r = 0, c = 0;
@@ -79,15 +128,15 @@ int main()
 
     int** matrix = nullptr;
     try {
-        matrix = create(5, 5);
+        matrix = create(r, c);
     }
-    catch (const std::bad_alloc& e)
+    catch (const std::bad_alloc&e)
     {
         std::cerr << e.what() << "\n";
         return 1;   
     }
     std::cout << "created!\n";
-    construct(matrix, 2, 5, 5);
+    construct(matrix, 2, r, c);
     std::cout << matrix[0][0] << "\n"; // undefined behavior (UB)
     input(matrix, r, c);
     if (!std::cin)
@@ -97,6 +146,5 @@ int main()
         return 1;
     }
     output(matrix, r, c);
-      
     destroy(matrix, r);
 }
