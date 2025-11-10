71e58c5c27df02b1de946dc3ec8172025de6d719
elizazha <lizokbella@mail.ru>
2025-11-10 17:09:04 +0300

    3 задачка, исп доп память

--- 10-11.cpp
+++ 10-11.cpp
@@ -1,30 +1,23 @@
-#include <iostream> //ракссчитать определитель матрицы, написать демонстрационную пограмму
+#include <iostream> //ракссчитать определитель матрицы, использя дополнительную память b. написать демонстрационную пограмму
 int * copy(int * a, size_t k, int * b){
   while (k--){
     *(++a)=*(++b);
   }
   return b;
 }
-int det(int * lhs, size_t n){ // lhs - left hand side(левый операнд), rhs - (правый операнд)
-  if (!n){return 0;}
-  else if( n == 1){return lhs[0]*lhs[3]-lhs[1]*lhs[2];}
+int det(int * b,  int * lhs, size_t n){ // lhs - left hand side(левый операнд), rhs - (правый операнд)
   int d = 0;
-  for (size_t  l = 0; l < n; ++l){
-    int pivot = lhs[l];
-    int * minor = new int [(n-1)*(n-1)];
+  for (size_t l = 0; l < n; ++l){
+    int * minor = b;
     for (size_t i = 1; i < n; ++i){
-      int * lhs_row = lhs + i*n;
+       int * lhs_row = lhs + i*n;
       int * min_row = minor + (i-1)*(n-1);
-      copy(lhs_row, l, min_row);
-      lhs_row +=(1+1);
-      copy(lhs_row, n-l+1, min_row);
-    }   
-    try{
-      d += pivot*det(minor, n-1)*((l%2)?-1:1);
-    }catch(...){
-      delete[] minor;
-      throw;
+      min_row = copy(lhs_row, l, min_row);
+      lhs_row += (l+1);
+      copy (lhs_row, n-(l+1), min_row);
     }
-
+    int * next = b + (n-1)*(n-1);
+    d += lhs[l]*det(next, minor, n-1)*(l%2?-1:1);
   }
+  return d;
 }
