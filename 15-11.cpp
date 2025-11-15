09b296a320f1fb6db063ba91e2ad9e03de0c8ea2
elizazha <lizokbella@mail.ru>
2025-11-15 12:36:27 +0300

    разобрали первую задачу

--- 15-11.cpp
+++ 15-11.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 
-// int * sum(size_t n, size_t m){  // задача 1. Код 2
+// int * sum(size_t n, size_t m, const int * a, const int * b){  // задача 1. Код 2
 //   int * r = new int[m*n];
 //   sum(n, m, r, a, b); // в r(result) скалдываем a и b
 //   return r;
 // }
+
+// void * sum(size_t n, size_t m, int * c, const int * a, const int * b){
+//   for (size_t i =0; i < m*n; i++){
+//       c[i]=a[i]+b[i];
+//     }
+// } // задача 1. Код 1
 
 // void add(size_t n, size_t m){ // - задача 1. Код 3
 //   sum(n, m, a, a, d) // a - куда запис рез( по сути с, но *а и *с указ на одно и то же место); а, b - то, что надо сложить
